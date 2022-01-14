c178c2cc499b3808ae8ffbd36dab5f4d32d7cda5
Kollurupranathi <kpranathi009@gmail.com>
2022-01-14 21:25:53 +0530

    pop

--- Steque.java
+++ Steque.java
@@ -81,6 +81,10 @@ public class Steque<Item> implements Iterable<Item> {
      * @return Item object from steque.
      */
     public Item pop() {
+        if(isEmpty()) throw new NoSuchElementException();
+        Item item = first.item;
+        first = first.next;
+        return item;
 
     }
     
